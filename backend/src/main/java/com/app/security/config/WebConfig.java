a3f29e51d3169866576ace56c97d912553ec8535
mariusss11 <mariuscarchilan07@gmail.com>
2025-09-24 22:06:31 +0300

    Solve the issue with cors

---
+++ backend/src/main/java/com/app/security/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.app.security.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.allowed-origins}")
+    private String allowedOriginsRaw;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = allowedOriginsRaw.split(",");
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
